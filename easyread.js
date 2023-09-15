613c4051b00b21341516361eabba720cd1f7dd6a
pmadhusudhan <madhutemp@gmail.com>
2023-09-15 09:14:56 -0400

    Update easyread.js

--- easyread.js
+++ easyread.js
@@ -10,7 +10,7 @@ script.addEventListener('load', function() {
   var documentClone = document.cloneNode(true); 
   var article = new Readability(documentClone).parse();
   var d = document.createElement("div");
-  d.innerHTML = "<button id='openAI'>AI</button>";
+  d.innerHTML = "<button id='openAI' style='padding:5px;background:black;color:white'>AI</button>";
   d.setAttribute("id","aicontainer");
   d.innerHTML += article.content;
   d.setAttribute("style","background:white;padding:20px;font-size:20px;border:2px orange solid;position:fixed;top:0px;right:0px;width:100%;height:800px;overflow:auto;z-index:999999;");
