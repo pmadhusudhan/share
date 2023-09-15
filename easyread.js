0e6e66e468d1e8cd02262f09609355bd23f0f046
pmadhusudhan <madhutemp@gmail.com>
2023-09-15 08:57:09 -0400

    Update easyread.js

--- easyread.js
+++ easyread.js
@@ -1,18 +1,12 @@
-/*var s = document.createElement("script");
-s.src = "https://code.jquery.com/jquery-3.7.1.min.js";
-document.getElementsByTagName("body")[0].appendChild(s);
-
-$(document).ready(function(){
- 
-alert("jquery ready");
-})
-*/
-s = document.createElement("script");
 s.src = "https://pmadhusudhan.github.io/interview/Readability.js";
 document.getElementsByTagName("body")[0].appendChild(s);
 window.setTimeout(function(){var documentClone = document.cloneNode(true); 
 var article = new Readability(documentClone).parse();
 var d = document.createElement("div");
-d.innerHTML = article.content;d.setAttribute("style","background:white;padding:20px;border:2px orange solid;position:fixed;top:0px;right:0px;width:100%;height:800px;overflow:auto;z-index:999999;");
+d.innerHTML = "<button id='openAI'>AI</button>";
+d.innerHTML += article.content;d.setAttribute("style","background:white;padding:20px;font-size:20px;border:2px orange solid;position:fixed;top:0px;right:0px;width:100%;height:800px;overflow:auto;z-index:999999;");
 document.getElementsByTagName("body")[0].appendChild(d);},1000);
 
+document.querySelector("#openAI").addEventListener("click", function() {
+  open("https://bard.google.com");
+});
