9a49452d2642f2b25665a06ae1a1bfdcf44343c0
pmadhusudhan <madhutemp@gmail.com>
2023-09-15 10:22:39 -0400

    Update easyread.js

--- easyread.js
+++ easyread.js
@@ -13,7 +13,7 @@ aiscript.addEventListener('load', function() {
   d.innerHTML = "<button id='openAI' style='padding:5px;background:black;color:white'>AI</button>";
   d.setAttribute("id","aicontainer");
   d.innerHTML += article.content;
-  d.setAttribute("style","background:white;padding:20px;font-size:20px;border:2px orange solid;position:fixed;top:0px;right:0px;width:100%;height:800px;overflow:auto;z-index:999999;");
+  d.setAttribute("style","background:white;padding:20px;font-size:20px;border:2px orange solid;position:fixed;top:0px;left:0px;width:95%;height:800px;overflow:auto;z-index:999999;");
   document.getElementsByTagName("body")[0].appendChild(d);
 
   document.getElementById("openAI").addEventListener("click", function() {
