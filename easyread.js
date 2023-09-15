eea79c16571cbd711774744f96675c395d089875
pmadhusudhan <madhutemp@gmail.com>
2023-09-15 08:17:25 -0400

    Create easyread.js

---
+++ easyread.js
@@ -0,0 +1,9 @@
+var s = document.createElement("script");
+s.src = "https://code.jquery.com/jquery-3.7.1.min.js";
+
+document.getElementsByTagName("body")[0].append(s);
+
+$(document).ready(function(){
+
+alert("jquery ready");
+})
