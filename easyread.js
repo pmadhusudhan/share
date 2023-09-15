d5e48894c17cd9b820c29c215d2e21bef69198e6
pmadhusudhan <madhutemp@gmail.com>
2023-09-15 08:47:57 -0400

    Update easyread.js

--- easyread.js
+++ easyread.js
@@ -13,6 +13,6 @@ document.getElementsByTagName("body")[0].appendChild(s);
 window.setTimeout(function(){var documentClone = document.cloneNode(true); 
 var article = new Readability(documentClone).parse();
 var d = document.createElement("div");
-d.innerHTML = article.content;d.setAttribute("style","background:white;padding:10px;border:2px orange solid;position:fixed;top:0px;right:0px;width:50%;height:800px;overflow:auto;z-index:999999;");
+d.innerHTML = article.content;d.setAttribute("style","background:white;padding:10px;border:2px orange solid;position:fixed;top:0px;right:0px;width:100%;height:800px;overflow:auto;z-index:999999;");
 document.getElementsByTagName("body")[0].appendChild(d);},1000);
 
