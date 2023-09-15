935babf86b5e8f3b9900b741c61d241fe6542648
pmadhusudhan <madhutemp@gmail.com>
2023-09-15 09:02:44 -0400

    Update easyread.js

--- easyread.js
+++ easyread.js
@@ -4,9 +4,12 @@ window.setTimeout(function(){var documentClone = document.cloneNode(true);
 var article = new Readability(documentClone).parse();
 var d = document.createElement("div");
 d.innerHTML = "<button id='openAI'>AI</button>";
-d.innerHTML += article.content;d.setAttribute("style","background:white;padding:20px;font-size:20px;border:2px orange solid;position:fixed;top:0px;right:0px;width:100%;height:800px;overflow:auto;z-index:999999;");
+d.setAttribute("id","aicontainer");
+d.setAttribute("style","background:white;padding:20px;font-size:20px;border:2px orange solid;position:fixed;top:0px;right:0px;width:100%;height:800px;overflow:auto;z-index:999999;");
 document.getElementsByTagName("body")[0].appendChild(d);},1000);
 
 document.querySelector("#openAI").addEventListener("click", function() {
+  
+  document.querySelector("#aicontainer").innerHTML =  article.content;
   open("https://bard.google.com");
 });
