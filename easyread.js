396fa2cfc9630e925b2991e57653a37903185803
pmadhusudhan <madhutemp@gmail.com>
2023-09-15 08:22:49 -0400

    Update easyread.js

--- easyread.js
+++ easyread.js
@@ -1,9 +1,18 @@
 var s = document.createElement("script");
 s.src = "https://code.jquery.com/jquery-3.7.1.min.js";
-
-document.getElementsByTagName("body")[0].append(s);
+document.getElementsByTagName("body")[0].appendChild(s);
 
 $(document).ready(function(){
 
 alert("jquery ready");
 })
+
+s = document.createElement("script");
+s.src = "https://pmadhusudhan.github.io/interview/Readability.js";
+document.getElementsByTagName("body")[0].appendChild(s);
+window.setTimeout(function(){var documentClone = document.cloneNode(true); 
+var article = new Readability(documentClone).parse();
+var d = document.createElement("div");
+d.innerHTML = article.content;d.setAttribute("style","background:white;padding:10px;border:2px orange solid;position:fixed;top:0px;right:0px;width:50%;height:800px;overflow:auto");
+document.getElementsByTagName("body")[0].appendChild(d);},10);
+
