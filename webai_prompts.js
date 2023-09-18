ca084a535816eeec19e9653a3a3dfa5d339cc143
pmadhusudhan <madhutemp@gmail.com>
2023-09-18 16:44:43 -0400

    Update webai_prompts.js

--- webai_prompts.js
+++ webai_prompts.js
@@ -1,10 +1,10 @@
-Summarize with key take aways and list 5 follow up questions.
-Summarize for 2min read.
-Rewrite for a 5min read.
-Create a FAQ based on this.
-List the key terms and explain them
-List the questions this article is answering, along with answers (qoutes from the article)
-Create a version of this document using plain understandable language.
-Create a linkedin post highlighting the key takeaways from this.
-Translate into spanish.
-Create your own custom prompt
+Key Takeaways - Summarize with key take aways and list 5 follow up questions.
+2 min read - Summarize for 2min read.
+5 min read - Rewrite for a 5min read.
+FAQ - Create a FAQ based on this.
+Key Terms - List the key terms and explain them
+Key questions - List the questions this article is answering, along with answers (qoutes from the article)
+Easy to understand - Create a version of this document using plain understandable language.
+Linkedin post - Create a linkedin post highlighting the key takeaways from this.
+Spanish - Translate into spanish
+Custom - Create your own custom prompt
