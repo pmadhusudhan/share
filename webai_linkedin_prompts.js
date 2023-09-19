188ae728c6ad5cc26047512c2b7a6497c2e74081
pmadhusudhan <madhutemp@gmail.com>
2023-09-19 10:41:43 -0400

    Create webai_linkedin_prompts.js

---
+++ webai_linkedin_prompts.js
@@ -0,0 +1,11 @@
+Profile him/her - What topics is this person posting about.
+Key Takeaways - Summarize with key take aways and list 5 follow up questions.
+2 min read - Summarize for 2min read.
+5 min read - Rewrite for a 5min read.
+FAQ - Create a FAQ based on this.
+Key Terms - List the key terms and explain them
+Key questions - List the questions this article is answering, along with answers (qoutes from the article)
+Easy to understand - Create a version of this document using plain understandable language.
+Linkedin post - Create a linkedin post highlighting the key takeaways from this.
+Spanish - Translate into spanish
+Custom - Create your own custom prompt
