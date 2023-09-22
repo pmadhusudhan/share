da90f5217b49b2f44a6f8bda0502512d488c1084
pmadhusudhan <madhutemp@gmail.com>
2023-09-22 13:55:07 -0400

    Update webai_prompts.js

--- webai_prompts.js
+++ webai_prompts.js
@@ -1,7 +1,7 @@
 Show Key Takeaways - Summarize with key take aways and list 5 follow up questions.
 Make it 2 min read - Summarize for 2min read.
 Make it 5 min read - Rewrite for a 5min read.
-Create slides - Summarize this into slides.
+Create slides - Summarize this into slides with bulleted text.
 Create FAQ - Create a FAQ based on this.
 List Key Terms - List the key terms and explain them
 List Key questions - List the questions this article is answering, along with answers (qoutes from the article)
